8d5a5b0e7543641c240aa0ebf0f38b2e2ab61d48
Aman7445 <ak858696@gmail.com>
2021-02-07 01:32:06 +0530

    small changes

--- stacks.c
+++ stacks.c
@@ -5,45 +5,45 @@
 int stack[size];
 int top=-1;
 
-// operating in one stack only
-// void push(int stack[],int data){
-//     if(top==size-1){
-//         printf("can't insert stack is full\n");
-//     }
-//     else
-//     {
-//         top++;
-//         stack[top]=data;
-//     }
+//operating in one stack only
+void push(int stack[],int data){
+    if(top==size-1){
+        printf("can't insert stack is full\n");
+    }
+    else
+    {
+        top++;
+        stack[top]=data;
+    }
     
-// }
+}
 
-// void pull(int stack[]){
-//     int del;
-//     if(top==-1){
-//         printf("stack is empty can't delete\n");
-//     }
-//     else
-//     {
-//         del=stack[top];
-//         top--;
-//         printf("deleted element is %d\n",del);
-//     }
+void pull(int stack[]){
+    int del;
+    if(top==-1){
+        printf("stack is empty can't delete\n");
+    }
+    else
+    {
+        del=stack[top];
+        top--;
+        printf("deleted element is %d\n",del);
+    }
     
-// }
+}
 
-// void display(int stack[]){
-//     int i;
-//     if(top==-1){
-//         printf("stack is empty\n");
-//     }
-//     else
-//     {
-//         for(i=top;i>=0;i--){
-//             printf("stack %d element is %d\n",i ,stack[i]);
-//         }
-//     }
-// }
+void display(int stack[]){
+    int i;
+    if(top==-1){
+        printf("stack is empty\n");
+    }
+    else
+    {
+        for(i=top;i>=0;i--){
+            printf("stack %d element is %d\n",i ,stack[i]);
+        }
+    }
+}
 
 int main(){
     int s[size];
