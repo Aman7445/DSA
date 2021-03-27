bbecfeafec2ae967439c7dbc244607892e0383f2
Aman7445 <ak858696@gmail.com>
2021-03-28 02:03:46 +0530

    cptemplate

---
+++ cptemplate.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define FL(i,a,b) for(int i=a;i<b;i++)
+#define vi vector<int>
+#define pb push_back
+#define pob pop_back
+#define all (a.begin,a.end)
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    return 0;
+
+}
