2ee89d9992ae729906d1822961a7fa0b22045c8d
Aman7445 <ak858696@gmail.com>
2021-07-29 20:39:52 +0530

    graph updated

--- CPP/graph.cpp
+++ CPP/graph.cpp
@@ -1,94 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
-std::vector<std::vector<std::pair<int, int>>> graph; //vector of vector of pair where pair represents edge and vertex
+using namespace std;
 
-std::vector<std::vector<int>> graph2;
-//with weight
-//for without weight use wt=0 for all edges
-void addEdge(int u, int v, bool bidirect = true, int wt = 0)
-{
-    graph[u].push_back(std::make_pair(v, wt));
-    if (bidirect)
-    {
-        graph[v].push_back(std::make_pair(u, wt));
-    }
-}
-//without weight
-void addEdge2(int u, int v, bool bidirect = true)
-{
-    graph2[u].push_back(v);
-    if (bidirect)
-    {
-        graph2[v].push_back(u);
-    }
-}
-void bfs(int src, int vertices)
-{
-    std::queue<int> que;
-    std::vector<bool> visited(vertices, false);
-    que.push(src);
-    visited[src] = true;
-    while (!que.empty())
-    {
-        int CurrentNode = que.front();
-        que.pop();
-        std::cout << CurrentNode << " ";
-        for (auto neighbour : graph[CurrentNode]) //neighbour is pair
-        {
-            if (not visited[neighbour.first])
-            {
-                que.push(neighbour.first);
-                visited[neighbour.first] = true;
-            }
-        }
-    }
-}
-void Display()
-{
-    for (auto i : graph)
-    {
-        if (i.size() == 0)
-        {
-            std::cout << "empty ";
-        }
+vector<vector<pair<int,int>>> graph;
 
-        for (auto j : i)
-        {
-            std::cout << "{" << j.first << " , " << j.second << "}";
-        }
-        std::cout << "\n";
-    }
-}
-void Display2()
-{
-    for (auto i : graph2)
-    {
-        if (i.size() == 0)
-        {
-            std::cout << "empty"
-                      << " ";
-        }
-        for (auto j : i)
-        {
-            std::cout << "{" << j << "}";
-        }
-        std::cout << "\n";
-    }
-}
+//weighted graph 
+void addEdge(int u,int v,int wgt){
+    graph[u].push_back(make_pair(v,wgt));
+    graph[v].push_back(make_pair(u,wgt));
 
-int main()
-{
-    int vertices, edges;
-    std::cin >> vertices >> edges;
-    graph2.resize(vertices); //giving the size of the graph
-    while (edges--)
-    {
-        int u, v, wt;
-        std::cin >> u >> v;
-        // addEdge(u, v, true, wt);
-        addEdge2(u, v, true);
+}
+void Display(){
+    for(auto i: graph){
+        if(i.size()==0){
+            cout<<"Empty";
+        }
+        for(auto j :i){
+            cout<<"{ "<<j.first<<", "<<j.second<<" }";
+        }
+        cout<<"\n";
     }
-    Display2();
+    
+}
+int main(){
+    int v,e;
+    cin>>v>>e;
+    graph.resize(v+1);
+    while(e--){
+        int u,v,wgt;
+        cin>>u>>v>>wgt;
+        addEdge(u,v,wgt);
+    }
+    Display();
 }
 //input format
 // 6 7  no of vertices and edges
