339807fabb2a24eacacff6993147f0157eef6d7d
Aman7445 <ak858696@gmail.com>
2021-08-23 02:21:30 +0530

    template update

--- CPP/cptemplate.cpp
+++ CPP/cptemplate.cpp
@@ -2,18 +2,22 @@
 
 using namespace std;
 
-#define ll long long int
-#define FL(i,a,b) for(int i=a;i<b;i++)
-#define vi vector<int>
-#define pb push_back
-#define pob pop_back
-#define all (a.begin,a.end)
+#define ll long long int 
+#define vi vector<int> 
+#define pb push_back 
 #define vll vector<long long> 
 #define umii unordered_map<int,int> 
+#define FL(i,a,b) for(int i=a;i<b;i++)
+#define and &&
+#define or ||
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
+    int t;
+    while(t--){
+        
+    }
 
     return 0;
 
