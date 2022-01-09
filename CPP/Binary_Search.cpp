2cadb39ab191e4b1567eca4c6d015327ace1448f
Aman7445 <ak858696@gmail.com>
2022-01-10 03:53:53 +0530

    order_agnostic binary search

--- CPP/Binary_Search.cpp
+++ CPP/Binary_Search.cpp
@@ -14,9 +14,28 @@ int bs(vector<int> arr,int target){
     return -1;
 }
 
+int order_agnostic_bs(vector<int> arr,int target){
+    int start=0,end=arr.size()-1;
+    int mid;
+    bool isAsc=arr[start]<arr[end];
+    while(start<=end){
+        mid=start+(end-start/2);
+        if(arr[mid]==target) return mid;
+        if(isAsc){
+            if(target>arr[mid]) start=mid+1;
+            else end=mid-1;
+        }
+        else{
+            if(target>arr[mid]) end=mid-1;
+            else start=mid+1;
+        }
+    }
+    return -1;
+}
+
 int main(){
-    vector<int> arr={2,30,35,55,90,98};
+    vector<int> arr={98,90,78,65,55,43,23};
     int target=90;
-    cout<<bs(arr,target);
+    cout<<order_agnostic_bs(arr,target);
     return 0;
 }
