d8847e01cc74a18ff8c35bd6b96ff51400522c20
Aman7445 <ak858696@gmail.com>
2022-02-22 00:53:16 +0530

    knapsack updated

--- CPP/DP/knapsack.cpp
+++ CPP/DP/knapsack.cpp
@@ -3,51 +3,46 @@ using namespace std;
 
 //Memoization Technique
 vector<vector<int>> knp(10,vector<int>(15,-1)); 
-vector<vector<int>> dp(5,vector<int>(8)); 
-int knapsack(int val[],int wt[],int we,int n){
+
+int knapsack(int val[],int wt[],int W,int n){
     if(n==0){
         return 0;
     }
-    if(knp[n][we]!=-1){
-        return knp[n][we];
+    if(knp[n][W]!=-1){
+        return knp[n][W];
     }
-    if(wt[n-1]<=we){
-        return knp[n][we]=max(val[n-1]+knapsack(val,wt,we-wt[n-1],n-1),knapsack(val,wt,we,n-1));
+    if(wt[n-1]<=W){
+        return knp[n][W]=max(val[n-1]+knapsack(val,wt,W-wt[n-1],n-1),knapsack(val,wt,W,n-1));
 
     }
     else{
-        return knp[n][we]=knapsack(val,wt,we,n-1);
+        return knp[n][W]=knapsack(val,wt,W,n-1);
     }
-    return knp[n][we];
 }
-
-//tabulation
-int tknapsack(int val[],int wt[],int we,int n){
-    
-    for(int i=0;i<n+1;i++){
-        for(int j=0;j<we+1;j++){
+int tknapsack(int val[],int wt[],int W,int n,vector<vector<int>> t){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=W;j++){
             if(i==0||j==0){
-                dp[i][j]=0;
+                t[i][j]=0;
             }
-            else if(wt[i-1]<=we){
-                dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+            else if(wt[i-1]<=j){
+                t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
             }
             else{
-                dp[i][j]=dp[i-1][j];
+                t[i][j]=t[i-1][j];
             }
         }
     }
-    return dp[n][we];
+    return t[n][W];
 }
-
 int main(){
-    int n=4;
+    int n=4,W=7;
     int val[n]={1,3,4,5};
     int wt[n]={1,4,5,7};
-    int we=7;
-    
-    int ans=knapsack(val,wt,we,n);
-    int ans2=tknapsack(val,wt,we,n);
+    // int t[n+2][W+2];
+    vector<vector<int>> t(n+1,vector<int>(W+1));
+    int ans=knapsack(val,wt,W,n);
+    int ans2=tknapsack(val,wt,W,n,t);
     cout<<ans<<"\n";
     cout<<ans2;
     // for(int i=0;i<p.size();i++){
