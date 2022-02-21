6e44dbbd1cbb929994e42eeeb4ad424694661272
Aman7445 <ak858696@gmail.com>
2022-02-21 23:38:46 +0530

    knapsack tabulation done

--- CPP/DP/knapsack.cpp
+++ CPP/DP/knapsack.cpp
@@ -3,53 +3,53 @@ using namespace std;
 
 //Memoization Technique
 vector<vector<int>> knp(10,vector<int>(15,-1)); 
-
-int knapsack(int val[],int wt[],int W,int n){
+vector<vector<int>> dp(5,vector<int>(8)); 
+int knapsack(int val[],int wt[],int we,int n){
     if(n==0){
         return 0;
     }
-    if(knp[n][W]!=-1){
-        return knp[n][W];
+    if(knp[n][we]!=-1){
+        return knp[n][we];
     }
-    if(wt[n-1]<=W){
-        return knp[n][W]=max(val[n-1]+knapsack(val,wt,W-wt[n-1],n-1),knapsack(val,wt,W,n-1));
+    if(wt[n-1]<=we){
+        return knp[n][we]=max(val[n-1]+knapsack(val,wt,we-wt[n-1],n-1),knapsack(val,wt,we,n-1));
 
     }
     else{
-        return knp[n][W]=knapsack(val,wt,W,n-1);
+        return knp[n][we]=knapsack(val,wt,we,n-1);
     }
-    return knp[n][W];
+    return knp[n][we];
 }
 
 //tabulation
-int tknapsack(int val[],int wt[],int W,int n,vector<vector<int>> t){
+int tknapsack(int val[],int wt[],int we,int n){
     
     for(int i=0;i<n+1;i++){
-        for(int j=0;j<W+1;j++){
+        for(int j=0;j<we+1;j++){
             if(i==0||j==0){
-                t[i][j]=0;
+                dp[i][j]=0;
             }
-            if(wt[i-1]<=W){
-                t[i][j]=max(val[i-1]+t[i-1][j-1],t[i-1][j]);
+            else if(wt[i-1]<=we){
+                dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
             }
             else{
-                t[i][j]=t[i-1][j];
+                dp[i][j]=dp[i-1][j];
             }
         }
     }
-    return t[n][W];
+    return dp[n][we];
 }
 
 int main(){
     int n=4;
     int val[n]={1,3,4,5};
     int wt[n]={1,4,5,7};
-    int W=7;
-    vector<vector<int>> t;
-    int ans=knapsack(val,wt,W,n);
-    // int ans2=tknapsack(val,wt,W,n,t);
+    int we=7;
+    
+    int ans=knapsack(val,wt,we,n);
+    int ans2=tknapsack(val,wt,we,n);
     cout<<ans<<"\n";
-    // cout<<ans2;
+    cout<<ans2;
     // for(int i=0;i<p.size();i++){
     //     for(int j=0;j<p[i].size();j++){
     //         cout<<p[i][j]<<" ";
